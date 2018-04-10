a96e739c8bfd684bfb92828718d49c9d3cbabac3
veeshalsoni <vishal@torcanes.com>
2018-04-10 09:02:38 +0530

    Corrected Recursion - printing downloaded files

--- github-dir-down.py
+++ github-dir-down.py
@@ -1,29 +1,37 @@
 import requests
 import os
 
-def clone_dir(contents,align,apiurl):
+def clone_dir(align,apiurl):
+	contents = requests.get(apiurl)
+
 	for content in contents.json():
 		#if directory download its content recursively
 		if content['type'] == 'dir':
-			apiurl = apiurl + "/" + content['name']
-			contents = requests.get(apiurl)
+			for i in range(align):
+				print "-",
+			print content['name']
 
 			parentdir = os.getcwd()
 			os.makedirs(content['name'])
 			os.chdir(content['name'])
-			clone_dir(contents,align+1,apiurl)
+			clone_dir(align+1,apiurl + "/" + content['name'])
 			os.chdir(parentdir)
 		#if file write it
 		else:
+			for i in range(align):
+				print "-",
+			print content['name']
 			fh = open(content['name'],'w')
 			rawfile = requests.get(content['download_url'])
 			fh.write(rawfile.content)
 			fh.close()
 
 #it works only for folders with https://github.com/:user/:repo/tree/master/:path
-path = raw_input("Path to clone")
+path = raw_input("Path to clone : ")
 args = path.split('/',7)
 apiurl = "https://api.github.com/repos/" + args[3] + "/" + args[4] + "/contents/" + args[7]
-contents = requests.get(apiurl)
-
-clone_dir(contents,0,apiurl)
+os.makedirs(args[7])
+os.chdir(args[7])
+print "Below files/directories are cloned"
+clone_dir(0,apiurl)
+print "Completed"
